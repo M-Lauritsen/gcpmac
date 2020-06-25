2ec98d39952f94e93915a1da93897a8e4915ce9f
Michael Lauritsen <i30288741@gmail.com>
2020-06-25 07:55:11 +0200

    adding unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
+describe("Paragraph test", () => {
+  test("render the paragraph on the page", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("p").text()).toContain(
+      "React + Google Cloud + GCP cloud build"
+    );
+  });
 });
+
+// describe("Paragraph test", () => {
+//   test("render the paragraph on the page", () => {
+//     const { getByText } = render(<App />);
+//     const linkElement = getByText("React + Google Cloud + GCP cloud build");
+//     expect(linkElement).toBeInTheDocument();
+//   });
+// });
