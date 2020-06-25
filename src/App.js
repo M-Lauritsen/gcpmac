787464852e760a691ea220fee13be8ecb556c47b
Michael Lauritsen <i30288741@gmail.com>
2020-06-25 10:16:35 +0200

    deploy med fejl

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React + Google Cloud + GCP cloud build</p>
+        <p>React + Google Cloud + GCP cloud buil</p>
         <a
           className="App-link"
           href="https://reactjs.org"
